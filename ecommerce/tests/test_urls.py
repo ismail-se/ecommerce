98f700ae4ecedd6b44d1e389e4a7c3d157f1dc9e
Soban Javed <iamsobanjaved@gmail.com>
2020-04-09 00:19:08 +0500

    fixed catalogue namespace error

--- ecommerce/tests/test_urls.py
+++ ecommerce/tests/test_urls.py
@@ -7,6 +7,12 @@ from ecommerce.tests.testcases import TestCase
 
 
 class TestUrls(TestCase):
+    def assert_redirect_to_lms(self, url):
+        response = self.client.get(url)
+        # Test client can't fetch external URLs, so fetch_redirect_response is set to
+        # False to avoid loading the final page
+        self.assertRedirects(response, get_lms_dashboard_url(), fetch_redirect_response=False)
+
     def test_unauthorized_redirection(self):
         """Test that users not authorized to access the Oscar front-end are redirected to the LMS dashboard."""
         user = self.create_user()
@@ -14,11 +20,7 @@ class TestUrls(TestCase):
         # Log in as a user not authorized to view the Oscar front-end (no staff permissions)
         success = self.client.login(username=user.username, password=self.password)
         self.assertTrue(success)
-
-        response = self.client.get(reverse('dashboard:index'))
-        # Test client can't fetch external URLs, so fetch_redirect_response is set to
-        # False to avoid loading the final page
-        self.assertRedirects(response, get_lms_dashboard_url(), fetch_redirect_response=False)
+        self.assert_redirect_to_lms(reverse('dashboard:index'))
 
     def test_api_docs(self):
         """
@@ -28,3 +30,22 @@ class TestUrls(TestCase):
         response = self.client.get(path)
 
         assert response.status_code == 200
+
+    def test_unauthorized_homepage_redirection(self):
+        """Test that users unauthorized to access the Oscar front-end are redirected to LMS Dashboard."""
+        user = self.create_user()
+        # Log in as a user not authorized to view the Oscar Dashboard (no staff permissions)
+        success = self.client.login(username=user.username, password=self.password)
+        self.assertTrue(success)
+        response = self.client.get('/')
+        self.assertRedirects(response, reverse('dashboard:index'), target_status_code=302)
+        self.assert_redirect_to_lms(response.url)
+
+    def test_authorized_homepage_redirection(self):
+        """Test that users authorized to access the Oscar front-end are redirected to Oscar Dashboard."""
+        user = self.create_user(is_staff=True)
+        # Log in as a user authorized to view the Oscar Dashboard (staff permissions)
+        success = self.client.login(username=user.username, password=self.password)
+        self.assertTrue(success)
+        response = self.client.get('/')
+        self.assertRedirects(response, reverse('dashboard:index'))
